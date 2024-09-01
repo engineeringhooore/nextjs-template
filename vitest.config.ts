5e94798547247ca8417efb621ddc4c222c19ac85
sistracia <156994197+sistracia@users.noreply.github.com>
2024-09-01 08:30:01 +0700

    Update dependencies, update `eslint` config, update `TypeScript` config, and setup test.

---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    restoreMocks: true,
+    setupFiles: ["./vitest.setup.ts"],
+    environmentMatchGlobs: [
+      // all component tests will run in happy-dom
+      ["**/?(*.)+(spec|test).[jt]sx", "happy-dom"],
+      // all function tests will run in node
+      ["**/?(*.)+(spec|test).[jt]s", "node"],
+    ],
+    coverage: {
+      provider: "v8",
+      reporter: ["clover", "json", "lcov", "text", "json-summary"],
+    },
+  },
+});
